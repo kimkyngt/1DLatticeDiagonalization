402c7f5dbb8275da3d65531db89d17f59016d024
Kyungtae Kim <61893865+kimkyngt@users.noreply.github.com>
2022-06-28 21:19:38 -0600

    checking wavefunction

--- scripts/calculate_shifts.jl
+++ scripts/calculate_shifts.jl
@@ -9,6 +9,9 @@ data_num = size(df)[1]
 depths = zeros(data_num)
 shifts = zeros(data_num)
 
+alpha_qm = -0.00125
+beta = -0.446e-6
+
 for ii in range(1, data_num)
 
     H_eigen = df[ii, "solution"]
@@ -22,9 +25,24 @@ for ii in range(1, data_num)
     sinkz_nz0 = get_sine_sq_exp(ψ_nz0, zz)
     sinkz_nz1 = get_sine_sq_exp(ψ_nz1, zz)
     
-    shifts[ii] = 0.0012*depths[ii]*(sinkz_nz1 - sinkz_nz0)/ustrip(fclock)
+    shifts[ii] = -alpha_qm*depths[ii]*(sinkz_nz1 - sinkz_nz0) + beta*depths[ii]*(2*sqrt(depths[ii]) -3)
 end
+I = sortperm(depths)
+depths = depths[I]
+shifts = shifts[I]
 
-
-# fig_spectrum = plot_eigen_spectrum(H_eigen)
-# Plots.pdf(fig_spectrum, plotsdir("spectrums.pdf"))
+fig = plot(
+    depths, 
+    shifts/ustrip(fclock) ,
+    yticks=[0, 1, 2, 3, 4, 5, 6]*1e-17, 
+    ylim=(-0.5e-17, 5e-17), 
+    dpi=300,
+    lw=2,
+    marker=:circle,
+    legend=false,
+    ylabel="Differential shift", 
+    xlabel="Lattice depth (Er)",
+    xticks=0:50:300,
+    # size=(400, 300),
+)
+Plots.pdf(fig, plotsdir("nz_modulation_lock_simulation.pdf"))
